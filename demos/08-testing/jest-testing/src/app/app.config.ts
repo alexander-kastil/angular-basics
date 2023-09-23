3c8ab2aa73a5830d135acd674c62d7e40946fc48
Alexander Pajer <alexander.pajer@integrations.at>
2023-09-23 18:32:53 +0200

    testing

--- demos/08-testing/jest-testing/src/app/app.config.ts
+++ demos/08-testing/jest-testing/src/app/app.config.ts
@@ -3,11 +3,8 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
-import { provideState, provideStore } from '@ngrx/store';
-import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { MarkdownModule } from 'ngx-markdown';
 import { routes } from './app.routes';
-import { appState } from './state/app.state';
 
 export const appConfig: ApplicationConfig = {
     providers: [
@@ -18,9 +15,5 @@ export const appConfig: ApplicationConfig = {
             MarkdownModule.forRoot(),
             MatSnackBarModule
         ]),
-        // Ngrx
-        provideStore(),
-        provideState(appState),
-        provideStoreDevtools(),
     ],
 };
