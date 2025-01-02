d65c5ca2b8e3e9c5becada486bc535ac40e45f41
Alexander Kastil <alexander.kastil@integrations.at>
2025-01-02 18:06:06 +0100

    refactor: remove obsolete components, models, and styles; clean up unused files

--- demos/09-securing-publishing/securing-api/Program.cs
+++ demos/09-securing-publishing/securing-api/Program.cs
@@ -1,12 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Builder;
 
 namespace ngDemoApp
 {
@@ -14,21 +14,75 @@ namespace ngDemoApp
     {
         public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
-            host.Run();
-        }
+            var builder = WebApplication.CreateBuilder(args);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args).ConfigureLogging((hostingContext, logging) =>
+            // Add services to the container
+            builder.Services.AddSingleton(builder.Configuration);
+
+            var fbProjectId = builder.Configuration["FirebaseProjectId"];
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.Authority = "https://securetoken.google.com/" + fbProjectId;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = "https://securetoken.google.com/" + fbProjectId,
+                        ValidateAudience = true,
+                        ValidAudience = fbProjectId,
+                        ValidateLifetime = true
+                    };
+                });
+
+            // Swagger configuration
+            builder.Services.AddSwaggerGen(c =>
             {
-                logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
-                logging.AddConsole();
-                logging.AddDebug();
-                logging.AddEventSourceLogger();
-            })
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ngDemo API", Version = "v1" });
             });
+
+            // CORS configuration
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("allowAll",
+                    builder => builder
+                        .SetIsOriginAllowed(host => true)
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials());
+            });
+
+            builder.Services.AddControllers();
+
+            // Configure logging
+            builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+            builder.Logging.AddConsole();
+            builder.Logging.AddDebug();
+            builder.Logging.AddEventSourceLogger();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ngDemo API");
+                c.RoutePrefix = string.Empty;
+            });
+
+            app.UseCors("allowAll");
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
     }
 }
