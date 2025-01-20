472843d600a97df92e43aa19d2fb111ec8783bec
Alexander Kastil <alexander.kastil@integrations.at>
2025-01-20 13:44:48 +0100

    Update setup script and add VS Code keybindings and settings

--- setup/setup-angular-dev.ps1
+++ setup/setup-angular-dev.ps1
@@ -1,7 +1,6 @@
 # Install winget packages
 Write-Host "Installing software using winget - 1/4" -ForegroundColor yellow
 
-winget install --id=Google.Chrome -e
 winget install --id=Microsoft.VisualStudioCode -e
 winget install --id=Git.Git -e
 winget install --id=GitHub.cli -e
@@ -16,18 +15,18 @@ $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";"
 # Install VS Code Extensions
 Write-Host "VS Code Extensions - 3/4" -ForegroundColor yellow
 
-code --install-extension ms-vscode.azurecli
-code --install-extension ms-vscode.azure-account
+code --install-extension angular.ng-template
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension GitHub.vscode-pull-request-github
-code --install-extension angular.ng-template
 code --install-extension 1tontech.angular-material
-code --install-extension mikael.angular-beastcode
 code --install-extension mdickin.markdown-shortcuts
 code --install-extension mhutchie.git-graph 
 code --install-extension hbenl.vscode-test-explorer
-code --install-extension raagh.angular-karma-test-explorer
+code --install-extension lucono.karma-test-explorer
 code --install-extension xabikos.jasminesnippets
+code --install-extension github.copilot
+code --install-extension redhat.vscode-yaml
+code --install-extension alex-pattison.theme-cobalt3
 
 # Install NVM
 Write-Host "Installing Node using NVM, Angular & json-server - 4/4" -ForegroundColor yellow
